16782219a106d3a6640a1d739c0fa8fc00189fda
erascardsilva <erasmo@vmi-informatica.com.br>
2023-05-07 23:56:04 -0300

    modified:   fatoria.php

--- fatoria.php
+++ fatoria.php
@@ -1,13 +1,13 @@
 <?php
 
-/*           Fatoria
+/*           Fatorial
 3) Crie um algoritmo que solicite um número, e faça o cálculo fatorial do mesmo,
  exiba o resultado na tela. Ex: Entrada = 3 Processamento: (3 * 2) * 1 Saída: 6 */
 
 echo "          Calculo Fatorial   \n";
 echo " Digite um numero para descobrirmos o fatorial do mesmo \n";
 
-$numero = readline(" Aqui coloque o numero :  ");
+$numero = readline(" Aqui coloque o numero :  ") ; 
 
 $fatorial = 1;
 for ($i = 1; $i <= $numero; $i++) {
